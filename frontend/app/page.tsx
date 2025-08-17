db96b97faa40512fb2c01c12d50845c659947b68
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-08-17 22:06:59 +0100

    stuff

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,6 @@
 // import Image from "next/image";
-
-import CustomCursor from "@/components/CustomCursor";
 import Hero from "@/components/Hero";
 import HowTo from "@/components/HowTo";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
--- frontend/providers/PrivyProviders.tsx
+++ frontend/providers/PrivyProviders.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 
-import { ReactNode } from 'react';
-import { PrivyProvider } from '@privy-io/react-auth';
+import { ReactNode } from "react";
+import { PrivyProvider } from "@privy-io/react-auth";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <PrivyProvider
-      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
       config={{
         embeddedWallets: {
-          ethereum: { createOnLogin: 'users-without-wallets' }
-        }
+          createOnLogin: "all-users",
+        },
       }}
     >
       {children}
