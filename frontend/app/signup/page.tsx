f75f7abd7027f4db7cded8eedabc207b5448471b
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-08-18 10:11:13 +0100

    made chnaged

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,9 +1,10 @@
-import PrivySignIn from '@/components/PrivySignIn';
+import Navbar from "@/components/Navbar";
+import PrivySignIn from "@/components/PrivySignIn";
 // import ProgressStepper from '@/components/ProgressStepper';
 // import { Step } from '@/types/steps';
 
 export default function SignUpPage() {
-  // const steps: Step[] = [ 
+  // const steps: Step[] = [
   //   { id: '01', name: 'Sign In', status: 'current' },
   //   { id: '02', name: 'Account Setup', status: 'upcoming' },
   //   { id: '03', name: 'Trustees', status: 'upcoming' },
@@ -11,13 +12,14 @@ export default function SignUpPage() {
   // ];
 
   return (
-    <div className="min-h-screen p-4 mt-20">
-      <div className="max-w-4xl mx-auto">
-        <div className="py-8">
-          {/* <ProgressStepper steps={steps} /> */}
+    <>
+    <Navbar />
+      <div className="min-h-screen p-4 mt-20">
+        <div className="max-w-4xl mx-auto">
+          <div className="py-8">{/* <ProgressStepper steps={steps} /> */}</div>
+          <PrivySignIn />
         </div>
-        <PrivySignIn />
       </div>
-    </div>
+    </>
   );
 }
