32195c5c32c0da49ba807ccda58a098ae6dc6d1c
dimka90 <dimkayilrit@gmail.com>
2026-01-06 17:46:32 +0100

    feat(contracts): implement recovery initiation and activation status

--- contracts/src/GenwealthVault.sol
+++ contracts/src/GenwealthVault.sol
@@ -18,10 +18,13 @@ contract GenwealthVault {
     // owner => trustee => isAuthorized
     mapping(address => mapping(address => bool)) public isTrustee;
 
-    event TrusteeAdded(address indexed owner, address indexed trustee);
+    event RecoveryInitiated(address indexed owner, address indexed initiator);
     event RecoveryCancelled(address indexed owner);
 
     error NotOwner();
+    error NotTrustee();
+    error StillActive();
+    error RecoveryAlreadyActive();
 
     /**
      * @dev Initialize or update vault settings.
@@ -60,5 +63,29 @@ contract GenwealthVault {
      */
     function removeTrustee(address _trustee) external {
         isTrustee[msg.sender][_trustee] = false;
+    }
+
+    /**
+     * @dev A trustee can initiate recovery if the owner has been inactive.
+     * @param _owner The address of the vault owner.
+     */
+    function initiateRecovery(address _owner) external {
+        if (!isTrustee[_owner][msg.sender]) revert NotTrustee();
+        if (vaults[_owner].recoveryActive) revert RecoveryAlreadyActive();
+        
+        uint256 lastTime = vaults[_owner].lastCheckIn;
+        uint256 period = vaults[_owner].inactivityPeriod;
+        
+        if (block.timestamp <= lastTime + period) revert StillActive();
+        
+        vaults[_owner].recoveryActive = true;
+        emit RecoveryInitiated(_owner, msg.sender);
+    }
+
+    /**
+     * @dev Check if recovery is active for a given owner.
+     */
+    function isRecoveryActive(address _owner) external view returns (bool) {
+        return vaults[_owner].recoveryActive;
     }
 }
